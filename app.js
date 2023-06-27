ab11d55a74b514296fe71fabdc7696e39a9402db
abutler911 <abutler911@gmail.com>
2023-06-27 14:22:40 -0600

    adding register routes

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const ejsLayouts = require("express-ejs-layouts");
 const connectDB = require("./config/db");
+const passport = require(passport);
+const LocalStrategy = require("passport-local");
+const session = require("express-session");
+const bcrypt = require("bcryptjs");
+const User = require("./models/User");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -9,19 +15,69 @@ connectDB();
 // Set view engine to ejs
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: false }));
+
 // Tell express to use ejs layouts
 app.use(ejsLayouts);
 app.use(express.static("public"));
 
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    User.findOne({ username: username }, function (err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user || !user.isValidPassword(password)) {
+        return done(null, false, { message: "Username or password is wrong" });
+      }
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user._id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
+
 app.get("/", (req, res) => {
   res.render("index", { pageTitle: "Home" });
+});
+
+app.post("/register", (req, res) => {
+  let { username, password } = req.body;
+  let user = new User({ username, password });
+  user
+    .save()
+    .then((user) => {
+      return res.redirect("/");
+    })
+    .catch((err) => {
+      return res.send("Error registering user");
+    });
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { pageTitle: "About" });
 });
 
-app.get("/stageandspotlight", (req, res) => {
+app.get("/carriescareer", (req, res) => {
   const videos = [
     { thumbnail: "/imgs/carrie4.jpg", url: "/path/to/video1.mp4" },
     { thumbnail: "/imgs/carrie4.jpg", url: "/path/to/video1.mp4" },
@@ -32,9 +88,9 @@ app.get("/stageandspotlight", (req, res) => {
     { thumbnail: "/imgs/carrie4.jpg", url: "/path/to/video1.mp4" },
     { thumbnail: "/imgs/carrie4.jpg", url: "/path/to/video1.mp4" },
   ];
-  res.render("stageAndSpotlight", {
+  res.render("carriescareer", {
     videos: videos,
-    pageTitle: "Stage and Spotlight",
+    pageTitle: "Carries Career",
   });
 });
 
--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
   firstname: {
@@ -22,6 +23,15 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+});
+
+UserSchema.methods.isValidPassword = function isValidPassword(password) {
+  return bcrypt.compareSync(password, this.password);
+};
+
+UserSchema.pre("save", function preSave(next) {
+  this.password = bcrypt.hashSync(this.password, 10);
+  next();
 });
 
 module.exports = mongoose.model("User", UserSchema);
